323e7ac4b83591249366db7d0a7ba97dff189cff
oleksii.b <oleksii.b@dev.tagsoft.co>
2025-02-28 00:14:38 +0200

    resolved task

--- task.sql
+++ task.sql
@@ -2,10 +2,14 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
+INSERT INTO Orders(CustomerID, Date)
+VALUES(1, '2023-01-01');
 
--- Start the transaction 
-START TRANSACTION; 
+-- Start the transaction
+START TRANSACTION;
+INSERT INTO OrderItems(OrderID, ProductID, Count)
+VALUES(1, 1, 1);
 
--- And some data should be created inside the transaction 
-
-COMMIT; 
+-- And some data should be created inside the transaction
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
+COMMIT;
